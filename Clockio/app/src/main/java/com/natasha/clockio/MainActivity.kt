1d074a0685c8aa0f3616a580360e98007783279b
Nat <natasha.valentina1998@gmail.com>
2019-10-27 15:50:05 +0700

    material design

--- Clockio/app/src/main/java/com/natasha/clockio/MainActivity.kt
+++ Clockio/app/src/main/java/com/natasha/clockio/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.natasha.clockio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.natasha.clockio.login.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signInButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
